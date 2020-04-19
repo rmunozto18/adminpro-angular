845fc1165019d6f54801817693c0b7ac153b2023
rmunozto <rodrigolmtrs@gmail.com>
2020-04-18 23:35:42 -0500

    RLMT MAC - modulo personalizado y creacion de rutas hijas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,34 +6,20 @@ import { APP_ROUTES } from './app.routes';
 //Componntes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NotfoundpageComponent } from './shared/notfoundpage/notfoundpage.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { PagesModule } from './pages/pages.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NotfoundpageComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,12 +9,12 @@ import { RegisterComponent } from './login/register.component';
 
 
 const appRoutes:Routes = [
-    { path:'', component: PagesComponent, children: [
-        { path:'dashboard', component: DashboardComponent },
-        { path:'progress', component: ProgressComponent },
-        { path:'graficas1', component: Graficas1Component },
-        { path:'',redirectTo: '/dashboard', pathMatch:'full' }
-    ]},
+    // { path:'', component: PagesComponent, children: [
+    //     { path:'dashboard', component: DashboardComponent },
+    //     { path:'progress', component: ProgressComponent },
+    //     { path:'graficas1', component: Graficas1Component },
+    //     { path:'',redirectTo: '/dashboard', pathMatch:'full' }
+    // ]},
     { path:'login', component: LoginComponent },
     { path:'register', component: RegisterComponent },
     { path:'**', component: NotfoundpageComponent }
