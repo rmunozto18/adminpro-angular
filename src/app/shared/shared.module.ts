9d4ee3dd2e7c5f6d7c439b61d7a5d2bf980a82d3
rmunozto <rodrigolmtrs@gmail.com>
2020-05-14 00:15:49 -0500

    Se agrega funcionalidad de ruteo y se consume dummy para el sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';//este modulo incluye el uso de los ngIf ngFor
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { NotfoundpageComponent } from './notfoundpage/notfoundpage.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
+    imports: [
+        RouterModule,
+        CommonModule
+    ],
     declarations: [
         HeaderComponent,
         SidebarComponent,
